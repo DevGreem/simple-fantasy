2c698d34a1c182b4c0c70ceb9eef61242098fbef
DevGreem <ian.pichardo@hotmail.com>
2026-04-29 16:41:20 -0400

    feat: Changed combat initializer

--- test_anim_bug.gd
+++
@@ -1,3 +0,0 @@
-extends SceneTree
-func _init():
-    pass
--- test_super.gd
+++
@@ -1,7 +0,0 @@
-class_name A extends Node
-func _ready():
-    print("A ready")
-
-class_name B extends A
-func _ready():
-    print("B ready")
